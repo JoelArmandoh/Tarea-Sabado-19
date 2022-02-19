e29b3c67566eeefea905816fae47646d9777790d
JoelArmandoh <96342144+JoelArmandoh@users.noreply.github.com>
2022-02-19 05:55:43 -0600

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,23 @@
 function IMC(){
 
-nombre= document.getElementById("name").value;
-numero1= document.getElementById("num1").value;
-numero2= document.getElementById("num2").value;
+let nombre= document.getElementById("name").value;
+let numero1= document.getElementById("num1").value /100;
+let numero2= document.getElementById("num2").value;
+
 res="";
 
-res= (Math.pow(numero1,2)) (numero1/numero2);
-Resultado= document.getElementById("btn").innerHTML=res;
-alert("Tu nombre es" + numero1 + res);
+let imc = numero1 / numero2 ** 2;
 
-
-
-}
+    if (imc < 18.5) {
+      res="Esta en su peso magro"
+    } else if (imc < 24.9) {
+      res=" Esta en su peso normal"
+    } else if (imc < 29.9) {
+      res="Esta está com sobrepeso"
+    } else if (imc < 39.9) {
+      res="Esta con obesidad"
+    } else if (imc > 39.9) {
+      res="Esta con muuucha obesidad "
+    }
+    document.getElementById("resultados").innerText=  nombre +" " + res ; 
+  }
